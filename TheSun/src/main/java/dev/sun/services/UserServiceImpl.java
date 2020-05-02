e6ade65687f4a6f4b61c47a4815fb8f8832be93e
YoTNT <pikaqiujian@gmail.com>
2020-05-02 15:49:40 -0400

    Implement all essential service and controller methods for User entity with JUNIT testing for service and Postman testing for controller

--- TheSun/src/main/java/dev/sun/services/UserServiceImpl.java
+++ TheSun/src/main/java/dev/sun/services/UserServiceImpl.java
@@ -15,9 +15,49 @@ public class UserServiceImpl implements UserService{
 
 	@Autowired
 	UserRepository ur;
+	
 	@Override
-	public List<User> gellAllUsers() {
+	public List<User> getAllUsers() {
 		return (List<User>) ur.findAll();
+	}
+	
+	@Override
+	public User createUser(User user) {
+		user.setUserId(0);
+		user = ur.save(user);
+		return user;
+	}
+	
+	@Override
+	public User getUserById(int id) {
+		return ur.findById(id).get();
+	}
+	
+	
+	@Override
+	public User loginUser(String username, String password) {
+		User user = ur.findByUserName(username);
+		String userPassword = user.getPassword();
+		if(userPassword.equals(password)) {
+			return user;
+		}
+		else {
+			return null;	// Return null when the password is unmatched
+		}
+	}
+	
+	
+	@Override
+	public User updateUser(User user) {
+		ur.save(user);
+		return user;
+	}
+	
+	
+	@Override
+	public boolean deleteUser(User user) {
+		ur.delete(user);
+		return true;
 	}
 
 }
