8d62516c46981be8dce1de4b0e02c0fcf67a063f
unknown <wsd81_2@hotmail.com>
2020-05-04 10:34:12 -0400

    add @CrossOrigin to controller

--- TheSun/src/main/java/dev/sun/controllers/TicketController.java
+++ TheSun/src/main/java/dev/sun/controllers/TicketController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,8 @@ import dev.sun.services.TicketServiceImpl;
 
 @Controller
 @Component
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 public class TicketController {
 
 	@Autowired
