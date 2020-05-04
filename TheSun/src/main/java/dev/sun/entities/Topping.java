b03d70ea87f717d11d7f3a5c36e0903e0c1d6530
unknown <wsd81_2@hotmail.com>
2020-05-04 05:51:48 -0400

    Update files

--- TheSun/src/main/java/dev/sun/entities/Topping.java
+++ TheSun/src/main/java/dev/sun/entities/Topping.java
@@ -5,10 +5,12 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +28,7 @@ public class Topping {
 	@Column(name = "topping_cost")
 	private double toppingCost;
 	
+
 	public Topping() {
 		super();
 	}
